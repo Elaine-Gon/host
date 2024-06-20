c2789e77dd797edbb01f1451d8a60a913d2eba4f
Elaine Gonçalves de Oliveira <elaine.a.de.oliveira@avanade.com>
2024-06-20 04:05:29 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { startTransition } from 'react';
+import { Route, BrowserRouter, Routes, useNavigate } from 'react-router-dom';
+
+const Checkout = React.lazy( () => import("mfe/HomeMfe"));
+
+function Home() {
+  const navigate = useNavigate();
+
+  function handle() {
+    startTransition( () => {
+      navigate("/test") 
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={handle}>
+        Teste
+      </button>
+   </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={Home}/> 
+        <Route path='/test' Component={Checkout}/> 
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ src/remoteTypes.d.ts
@@ -0,0 +1,7 @@
+///<reference types="react" />
+
+declare module "mfe/HomeMfe" {
+    const HomeApp: React.ComponentType;
+
+    export default HomeApp;
+}
